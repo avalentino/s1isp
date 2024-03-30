fd4229c2fa634dc64175e87d9e61e97c093e4b42
Antonio Valentino <antonio.valentino@tiscali.it>
2024-03-30 09:04:42 +0100

    New l0index.py module

---
+++ s1isp/l0index.py
@@ -0,0 +1,21 @@
+"""Function and classes for the management of L0 index files.
+
+See Section 3.3.1.1.4 in S1PD.SP.00110.ASTR]_ Issue 1.8.
+"""
+
+import bpack
+import bpack.st
+from bpack import EBaseUnits, EByteOrder, T
+
+
+@bpack.st.codec
+@bpack.descriptor(size=36, byteorder=EByteOrder.BE, baseunits=EBaseUnits.BYTES)
+class BlockDescriptor:
+    """Block descriptor for L0 index files."""
+
+    data_and_time: T["f8"]
+    delta_time: T["f8"]
+    delta_size: T["u4"]
+    data_units_offset: T["u4"]
+    byte_offset: T["i8"]
+    variable_size_flag: bool
